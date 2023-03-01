e714c97e6893b7ad307b5c22639741ab08361445
Shafayet Jamil <shafayetemon88@gmail.com>
2023-03-01 15:40:44 +0600

    exercise-6 done

---
+++ exercise-6.rb
@@ -0,0 +1,12 @@
+no_of_worldcup = 5
+
+x = "Brazil has won #{no_of_worldcup} FIFA World Cup titles."
+y = 'This is denoted by five stars above the national team crest on their jerseys.'
+
+puts x
+puts y
+
+p = 'The Selecao won their first World Cup in 1958.'
+q = 'They won their most recent World Cup title in 2002.'
+
+puts p + q
