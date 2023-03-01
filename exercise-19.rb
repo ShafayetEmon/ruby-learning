6b79f35120c20e2c7f8d644fc5877ac59310a71e
Shafayet Jamil <shafayetemon88@gmail.com>
2023-03-01 18:52:23 +0600

    exercise-19 done

---
+++ exercise-19.rb
@@ -0,0 +1,20 @@
+def sum(arg1, arg2)
+    puts "Summation = #{arg1 + arg2}"
+end
+
+def sub(arg1, arg2)
+    puts "Subtraction = #{arg1 - arg2}"
+end
+
+def mul(arg1, arg2)
+    puts "Multiplication = #{arg1 * arg2}"
+end
+
+def div(arg1, arg2)
+    puts "Division = #{arg1 / arg2}"
+end
+
+sum(10, 10)
+sub(10, 10)
+mul(10, 10)
+div(10, 10)
