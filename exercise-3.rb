5a0c6dcacbc9cd7ac92832997b4a0f87aa89e010
Shafayet Jamil <shafayetemon88@gmail.com>
2023-03-01 14:55:00 +0600

    exercise-3 done

---
+++ exercise-3.rb
@@ -0,0 +1,17 @@
+#Summation
+puts "10 + 10 = #{10+10}"
+
+#Substraction
+puts "10 - 10 = #{10-10}"
+
+#MUltiplication
+puts "10 * 10 = #{10*10}"
+
+#Divisiom
+puts "10 / 10 = #{10/10}"
+
+#Floating point number
+height = 3.5
+width = 5.2
+
+puts "Area: #{height*width}"
